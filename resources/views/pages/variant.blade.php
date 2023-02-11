c2bebc8b6ea781dca8e4b4842f8816219f40972d
ahmadsakur <sakuretsuu@gmail.com>
2023-02-12 01:56:07 +0700

    CRUD Variants

--- resources/views/pages/variant.blade.php
+++ resources/views/pages/variant.blade.php
@@ -26,52 +26,37 @@
                         <table class="table align-items-center mb-0" id="variantDatatable">
                             <thead>
                                 <tr>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author
-                                    </th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                                        Function
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-8">No
                                     </th>
                                     <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Technology</th>
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-8">
+                                        Product Name</th>
                                     <th
-                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Employed</th>
-                                    <th class="text-secondary opacity-7"></th>
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-8">
+                                        Variant Name</th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-8">
+                                        Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {{-- Todo : Render from table --}}
-                                <tr>
-                                    <td>
-                                        <div class="d-flex px-2 py-1">
-                                            <div>
-                                                <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-2.jpg"
-                                                    class="avatar avatar-sm me-3">
-                                            </div>
-                                            <div class="d-flex flex-column justify-content-center">
-                                                <h6 class="mb-0 text-xs">John Michael</h6>
-                                                <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p class="text-xs font-weight-bold mb-0">Manager</p>
-                                        <p class="text-xs text-secondary mb-0">Organization</p>
-                                    </td>
-                                    <td class="align-middle text-center text-sm">
-                                        <span class="badge badge-sm badge-success">Online</span>
-                                    </td>
-                                    <td class="align-middle text-center">
-                                        <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
-                                    </td>
-                                    <td class="align-middle">
-                                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs"
-                                            data-toggle="tooltip" data-original-title="Edit user">
-                                            Edit
-                                        </a>
-                                    </td>
-                                </tr>
+                                @foreach ($variants as $variant)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td class="text-center">{{ $variant->product->name }}</td>
+                                        <td class="text-center">{{ $variant->variant_name }}</td>
+                                        <td class="text-center">
+                                            <button class="btn btn-sm btn-outline-info" data-bs-toggle="modal"
+                                                data-bs-target="#updateVariantModal" id="editVariantButton"
+                                                data-id="{{ $variant->id }}">Edit</button>
+                                            <button class="btn btn-sm btn-danger" data-bs-toggle="modal"
+                                                data-bs-target="#deleteVariantModal" id="deleteVariantButton"
+                                                data-id="{{ $variant->id }}">Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
                             </tbody>
                         </table>
                     </div>
@@ -93,15 +78,30 @@
                         @csrf
                         <div class="modal-body">
                             <div class="mb-3">
+                                <label for="product">Product</label>
+                                <select id="product_id" class="form-select" name="product_id"
+                                    aria-label=".form-select-sm example" name="product" required>
+                                    <option value="" selected>Select Product</option>
+                                    @foreach ($products as $product)
+                                        <option value="{{ $product->id }}">{{ $product->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="mb-3">
                                 <label for="name" class="form-label">Name</label>
                                 <input type="text" class="form-control" id="name" name="name"
                                     aria-describedby="slugHelp" name="name" autocomplete="off" required
-                                    placeholder="eg, coffee">
+                                    placeholder="ice, hot, extra egg">
                             </div>
-                            <div class="form-floating">
-                                <textarea class="form-control" name="description" placeholder="insert variant description" id="description"
-                                    style="height: 100px" required autocomplete="off"></textarea>
-                                <label for="description">Description</label>
+                            <div class="mb-3">
+                                <label for="price">Price</label>
+                                <div class="position-relative">
+                                    <input type="number" class="form-control" style="padding-left: 2.5rem;" step=""
+                                        name="price" id="price" required autocomplete="off">
+                                    </input>
+                                    <span class="position-absolute text-sm font-weight-bold"
+                                        style="top:9px; left:12px;">Rp</span>
+                                </div>
                             </div>
                         </div>
                         <div class="modal-footer">
@@ -114,8 +114,8 @@
         </div>
 
         {{-- Update Modal --}}
-        <div class="modal fade" id="updateVariantModal" tabindex="-1" role="dialog" aria-labelledby="updateVariantModal"
-            aria-hidden="true">
+        <div class="modal fade" id="updateVariantModal" tabindex="-1" role="dialog"
+            aria-labelledby="updateVariantModal" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -124,11 +124,22 @@
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <form role="form" action="{{ route('variant.update', 'update') }}" method="POST">
+                    <form role="form" method="POST" action="{{ route('variant.update', 'update') }}"
+                        id="variantUpdateForm">
                         @method('PATCH')
                         @csrf
                         <div class="modal-body">
                             <input type="hidden" name="id" id="edit-id">
+                            <div class="mb-3">
+                                <label for="product">Product</label>
+                                <select id="edit-product_id" class="form-select" name="product_id"
+                                    aria-label=".form-select-sm example" name="product" required>
+                                    <option value="" selected>Select Product</option>
+                                    @foreach ($products as $product)
+                                        <option value="{{ $product->id }}">{{ $product->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                             <div class="mb-3">
                                 <label for="name" class="form-label">Name</label>
                                 <input type="text" class="form-control" id="edit-name" name="name"
@@ -136,16 +147,14 @@
                                     placeholder="eg, coffee">
                             </div>
                             <div class="mb-3">
-                                <label for="slug" class="form-label">Slug</label>
-                                <input type="text" disabled readonly class="form-control" id="edit-slug"
-                                    name="slug" aria-describedby="slugHelp" name="slug" autocomplete="off"
-                                    required placeholder="eg, coffee">
-                                <span class="text-xxs text-info">slug will be automatically generated</span>
-                            </div>
-                            <div class="form-floating">
-                                <textarea class="form-control" name="description" placeholder="insert variant description" id="edit-description"
-                                    style="height: 100px" required autocomplete="off"></textarea>
-                                <label for="description">Description</label>
+                                <label for="price">Price</label>
+                                <div class="position-relative">
+                                    <input type="number" class="form-control" style="padding-left: 2.5rem;"
+                                        step="" name="price" id="edit-price" required autocomplete="off">
+                                    </input>
+                                    <span class="position-absolute text-sm font-weight-bold"
+                                        style="top:9px; left:12px;">Rp</span>
+                                </div>
                             </div>
                         </div>
                         <div class="modal-footer">
@@ -173,12 +182,12 @@
                         @method('DELETE')
                         @csrf
                         <div class="modal-body">
-                            <p>Anda Ingin Menghapus Data ?</p>
+                            <p>Do You want to Delete this product ?</p>
                             <input type="hidden" name="id" id="delete-id">
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-danger">Delete</button>
+                            <button type="submit" class="btn btn-danger">Continue</button>
                         </div>
                     </form>
                 </div>
@@ -190,29 +199,26 @@
     <script>
         // Edit variant modal
         $(document).on('click', 'button#editVariantButton', function() {
-            let id = $(this).data('edit');
+            let id = $(this).data('id');
             $.ajax({
                 type: "get",
                 url: 'variant/' + id,
                 dataType: 'json',
                 success: function(res) {
-                    console.log(res);
-
                     $('#edit-id').val(res[0].id);
-                    $('#edit-name').val(res[0].name);
-                    $('#edit-slug').val(res[0].slug);
-                    $('#edit-description').val(res[0].description);
-
+                    $('#edit-product_id option').filter(function() {
+                        return ($(this).val() == res[0].product_id);
+                    }).prop('selected', true);
+                    $('#edit-name').val(res[0].variant_name);
+                    $('#edit-price').val(res[0].price);
                 }
-
             })
-
         });
 
 
         // Delete variant modal
         $(document).on('click', 'button#deleteVariantButton', function() {
-            let deleteID = $(this).data('destroy');
+            let deleteID = $(this).data('id');
             $('#delete-id').val(deleteID);
         });
     </script>
